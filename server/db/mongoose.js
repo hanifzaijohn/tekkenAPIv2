a8fcb893b56ad4bfa24f1c25cba381fc3ff8ed5e
Johnny <hanifzaij@gmail.com>
2019-01-06 13:15:20 -0800

    Setup app for herko

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-const {password} = require('secrets');
+
 // use promises & connect to mongodb server
 mongoose.Promise = global.Promise;
-mongoose.connect((`mongodb://hanifzaij:${password}@ds047478.mlab.com:47478/tekkenmatchapi`, {useNewUrlParser: true}
-  || 'mongodb://localhost:27017/TekkenMatches', {useNewUrlParser: true}));
+
+//use mongodbURI (mongolab / heroku) or use local database 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TekkenMatches', {useNewUrlParser: true});
 
 // export mongoose
 module.exports = {mongoose};
