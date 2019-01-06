cfb0f4adb1c2c121bd1034a6ef4b2f0224dfd12b
Johnny <hanifzaij@gmail.com>
2019-01-06 12:56:49 -0800

    added gitignore file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
+const {password} = require('secrets');
 // use promises & connect to mongodb server
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TekkenMatches', {useNewUrlParser: true});
+mongoose.connect((`mongodb://hanifzaij:${password}@ds047478.mlab.com:47478/tekkenmatchapi`, {useNewUrlParser: true}
+  || 'mongodb://localhost:27017/TekkenMatches', {useNewUrlParser: true}));
 
 // export mongoose
 module.exports = {mongoose};
