9d7aeccb2c81861e2a4c7822e76e0a8ec83baf17
Johnny <hanifzaij@gmail.com>
2019-01-07 08:50:12 -0800

    first commit from laptop

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 /* sets up enviornment and database connection depending on what we our doing.
   if we are testing, we use test database, if we are developming we use our
   regular database. If we are producing we use the heroku clouddb */
+
+  // FOR FIRST COMMIT FROM LAPTOP
+  
 var env = process.env.NODE_ENV || 'development';
 console.log('env******************', env);
 
